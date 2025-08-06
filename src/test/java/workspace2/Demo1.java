cb1156ec78f21b447da5c26a92b530ad73f88ca4
shaik <shaik@VIDHYA>
2025-08-06 20:08:25 +0530

    demo committed

--- src/test/java/workspace2/Demo1.java
+++ src/test/java/workspace2/Demo1.java
@@ -1,10 +1,11 @@
-package demo;
+package workspace2;
 
-public class demo1 {
+public class Demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("bahubali");
+		
+		System.out.println("KGF");
 
 	}
 
